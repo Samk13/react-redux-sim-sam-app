bc75a2912d54c21085698388751bb19ec3906577
Sam Arbid <sam.arbid@devcode.se>
2021-02-15 16:39:53 +0100

    update readme add header component

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HeaderComponent.css'
+
+export default function HeaderComponent() {
+    return (
+        <div className="header">
+            <span className="menu">---</span>
+            <div>
+                React-redux App
+            </div>
+        </div>
+    )
+}
