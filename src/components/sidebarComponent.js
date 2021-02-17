bf8730c56b380cdf2b691babef7fa315d82bc68b
Sam Arbid <sam.arbid@devcode.se>
2021-02-17 10:54:42 +0100

    fix grid add navlink

--- src/components/sidebarComponent.js
+++ src/components/sidebarComponent.js
@@ -1,20 +1,29 @@
 
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styles from './sidebarComponent.module.css'
 export default function sidebarComponent() {
     return (
     <aside className={styles.aside}>
         <ul className={styles.unorderedList}>
             <li className={styles.list}>
-                <div>
-                    <Link className={styles.link} to="/">Home</Link>
-                </div>
+                <NavLink
+                    className={styles.link}
+                    activeClassName={styles.active}
+                    to="/" exact
+                >
+                    Home
+                </NavLink>
             </li>
             <li className={styles.list}>
-                <div>
-                    <Link className={styles.link} to="/redux">redux page</Link>
-                </div>
+
+                    <NavLink
+                        className={styles.link}
+                        activeClassName={styles.active}
+                        to="/redux"
+                    >
+                        redux page
+                    </NavLink>
             </li>
         </ul>
 
