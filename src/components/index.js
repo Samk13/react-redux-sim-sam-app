1f5d127be2130a8c50e8228d0b7c3e4bf9ea4c92
Samk13 <samarbid13@gmail.com>
2021-02-19 10:44:27 +0100

    feat: auto import components for cleaner imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,14 @@
-import ArticleListItem from './ArticleListItem'
-import FooterComponent from './FooterComponent'
-import HeaderComponent from './HeaderComponent'
+// Auto import all components from component folder resulting in cleaner way to import in the consumer component
+const requireContext = require.context('./', false, /.*\.(js|jsx)$/)
 
-export { ArticleListItem, FooterComponent, HeaderComponent }
+let components = {}
+
+requireContext.keys().forEach((fileName) => {
+  if (fileName === './index.js') return
+
+  let name = fileName.replace(/(\.\/|\.jsx|\.js)/g, '')
+
+  components[name] = requireContext(fileName).default
+})
+
+module.exports = components
