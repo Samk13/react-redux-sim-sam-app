1e110d7850dcc2c95cdf93b5506ae7ed9fe8a726
Samk13 <samarbid13@gmail.com>
2021-02-19 10:25:29 +0100

    fix: change components extentions to jsx

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import ArticleListItem from './ArticleListItem'
+import FooterComponent from './FooterComponent'
+import HeaderComponent from './HeaderComponent'
+
+export { ArticleListItem, FooterComponent, HeaderComponent }
