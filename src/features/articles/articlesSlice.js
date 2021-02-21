d80d0212eab06174a9857a1dc4a59a79df355b88
Samk13 <samarbid13@gmail.com>
2021-02-21 21:42:53 +0100

    feat: show articles, creat article list component

--- src/features/articles/articlesSlice.js
+++ src/features/articles/articlesSlice.js
@@ -19,15 +19,6 @@ const articleInitialState = [
     body:
       'I motsättning till vad många tror, är inte Lorem Ipsum slumpvisa ord. Det har sina rötter i ett stycke klassiskt litteratur på latin från 45 år före år 0, och är alltså över 2000 år gammalt. Richard McClintock, en professor i latin på Hampden-Sydney College i Virginia, översatte ett av de mer ovanliga orden, consectetur, från ett stycke Lorem Ipsum och fann dess ursprung genom att studera användningen av dessa ord i klassisk litteratur. Lorem Ipsum kommer från styckena 1.10.32 och 1.10.33 av "de Finibus Bonorum et Malorum" (Ytterligheterna av ont och gott) av Cicero, skriven 45 före år 0. Boken är en avhandling i teorier om etik, och var väldigt populär under renäsanssen. Den inledande meningen i Lorem Ipsum, "Lorem Ipsum dolor sit amet...", kommer från stycke 1.10.32.',
     seen: true
-  },
-  {
-    id: uuid(),
-    createdAt: new Date().toISOString(),
-    lastEdited: '',
-    author: 'Sam',
-    body:
-      'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
-    seen: false
   }
 ]
 
@@ -54,6 +45,7 @@ export const articleSlice = createSlice({
       const articleEdit = state.find((article) => article.id === action.payload.id)
       if (articleEdit) {
         articleEdit.body = action.payload.body
+        articleEdit.lastEdited = new Date().toDateString()
       }
     },
     remove: (state, { payload }) => {
@@ -66,5 +58,5 @@ export const articleSlice = createSlice({
 })
 
 export const { create, edit, remove } = articleSlice.actions
-export const show = (state) => state.articles
+export const showArticles = ({ articles }) => articles
 export default articleSlice.reducer
