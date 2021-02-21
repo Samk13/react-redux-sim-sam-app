e468e299b65074796d3048aa972eb3f239e74d7b
Samk13 <samarbid13@gmail.com>
2021-02-21 21:42:53 +0100

    feat: show articles, creat article list component

--- src/components/ArticleListItem.js
+++ src/components/ArticleListItem.js
@@ -1,5 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function ArticleListItem() {
-  return <div>article list item</div>
+ArticleListItem.propTypes = {
+  id: PropTypes.string,
+  author: PropTypes.string,
+  body: PropTypes.string,
+  createdAt: PropTypes.string,
+  seen: PropTypes.bool
+}
+
+export default function ArticleListItem(props) {
+  return (
+    <section>
+      <div>
+        <div>{props.author}</div>
+        <div>{props.body}</div>
+        <div>{props.createdAt}</div>
+        <div>{props.seen}</div>
+      </div>
+      <div>{props.seen ? 'seen' : 'Not seen yet'}</div>
+    </section>
+  )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,23 @@
-import { useDispatch } from 'react-redux'
-import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import React, { useState } from 'react'
 // import styles from './HomePage.module.css'
-import { create } from '../features/articles/articlesSlice'
+import { ArticleListItem } from '../components'
+import { create, showArticles } from '../features/articles/articlesSlice'
 import { useForm } from 'react-hook-form'
 
 export default function HomePage() {
   // const [article, setArticle] = useState('')
   const { register, handleSubmit, errors } = useForm()
   const dispatch = useDispatch()
+  const selectArticles = useSelector(showArticles)
   const onSubmit = (data) => {
     console.log(data)
     dispatch(create(data))
+  }
+  const [isEditMode, setIsEditMode] = useState(false)
+  const [editTodoInput, setEditTodoInput] = useState('')
+  const handleEditInputChange = (e) => {
+    setEditTodoInput(e.target.value)
   }
 
   return (
@@ -21,14 +28,14 @@ export default function HomePage() {
             type="text"
             placeholder="author"
             name="author"
-            ref={register({ required: true, minLength: 2, maxLength: 13 })}
+            ref={register({ required: true, minLength: 3, maxLength: 13 })}
           />
           {errors.author && <span>This field is required</span>}
           <input
             type="text"
             placeholder="body"
             name="body"
-            ref={register({ required: true, minLength: 2, maxLength: 1313 })}
+            ref={register({ required: true, minLength: 3, maxLength: 1313 })}
           />
           {errors.body && <span>This field is required</span>}
           <label htmlFor="article">article body</label>
@@ -41,6 +48,27 @@ export default function HomePage() {
           /> */}
           <button type="submit">Submit</button>
         </form>
+      </section>
+      <section>
+        <h1>all articles</h1>
+        <div>
+          <h2>
+            {selectArticles.map((article, index) => {
+              return (
+                <div key={article.id}>
+                  <ArticleListItem
+                    id={article.id}
+                    author={article.author}
+                    body={article.body}
+                    createdAt={article.createdAt}
+                    seen={article.seen}
+                  />
+                  <div>{article.seen ? 'seen' : 'Not seen yet'}</div>
+                </div>
+              )
+            })}
+          </h2>
+        </div>
       </section>
       {/* <section>
         <h1>articles</h1>
