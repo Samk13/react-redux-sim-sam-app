cf64c1cdbbbc6d4fbfac8b0c12432e5e7c3a4caf
Samk13 <samarbid13@gmail.com>
2021-02-22 09:25:54 +0100

    feat: add article listItem component

--- src/components/ArticleListItem.js
+++ src/components/ArticleListItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './articleListItem.module.css'
 import PropTypes from 'prop-types'
 
 ArticleListItem.propTypes = {
@@ -6,19 +7,23 @@ ArticleListItem.propTypes = {
   author: PropTypes.string,
   body: PropTypes.string,
   createdAt: PropTypes.string,
-  seen: PropTypes.bool
+  seen: PropTypes.bool,
+  image: PropTypes.string
 }
 
 export default function ArticleListItem(props) {
   return (
-    <section>
-      <div>
-        <div>{props.author}</div>
-        <div>{props.body}</div>
-        <div>{props.createdAt}</div>
-        <div>{props.seen}</div>
+    <article className={styles.articleContainer}>
+      <img className={styles.image} src={props.image} />
+      <div className={styles.textContainer}>
+        <h1>{props.author}</h1>
+        <p>{props.body}</p>
+        <div className={styles.articleFooter}>
+          <div>{props.createdAt}</div>
+          <div>{props.seen}</div>
+          <div>{props.seen ? 'seen' : 'Not seen yet'}</div>
+        </div>
       </div>
-      <div>{props.seen ? 'seen' : 'Not seen yet'}</div>
-    </section>
+    </article>
   )
 }
