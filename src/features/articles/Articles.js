ec133b63923569fdd2b420f19889ae6ae17c24a2
Samk13 <samarbid13@gmail.com>
2021-03-04 23:39:51 +0100

    :nail_care: add transition to add article

--- src/features/articles/Articles.js
+++ src/features/articles/Articles.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { ArticleForm, CardItem } from '../../components'
 import { useSelector } from 'react-redux'
 import styles from './articles.module.css'
 import { getArticles } from './articlesSlice'
+import * as ReactTranstionGroup from 'react-transition-group'
+import './transitions.css'
+const Transition = ReactTranstionGroup.CSSTransition
+const GroupTransition = ReactTranstionGroup.TransitionGroup
 
 export default function ArticleTest() {
   const selectArticles = useSelector(getArticles)
   const revMyArr = [].concat(selectArticles).reverse()
+  const nodeRef = useRef(null)
+
   return (
     <div>
       <div className={styles.formContainer}>
@@ -16,21 +22,28 @@ export default function ArticleTest() {
         <ArticleForm />
       </div>
       <div className={styles.container}>
-        {revMyArr?.map(({ id, lastEdited, createdAt, author, imgUrl, body, seen }) => {
-          return (
-            <div className={styles.cardContainer} key={id}>
-              <CardItem
-                lastEdited={lastEdited}
-                createdAt={createdAt}
-                author={author}
-                image={imgUrl}
-                body={body}
-                seen={seen}
-                id={id}
-              />
-            </div>
-          )
-        })}
+        <GroupTransition component={null}>
+          {revMyArr?.map(({ id, lastEdited, createdAt, author, imgUrl, body, seen }) => (
+            <Transition
+              nodeRef={nodeRef}
+              key={id}
+              timeout={{ enter: 800, exit: 500 }}
+              classNames="card-container"
+            >
+              <div ref={nodeRef}>
+                <CardItem
+                  lastEdited={lastEdited}
+                  createdAt={createdAt}
+                  author={author}
+                  image={imgUrl}
+                  body={body}
+                  seen={seen}
+                  id={id}
+                />
+              </div>
+            </Transition>
+          ))}
+        </GroupTransition>
       </div>
     </div>
   )
