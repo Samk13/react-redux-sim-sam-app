f719b122208b05cbb64a8ea63eb0c98172d22fa0
Sam Arbid <sam.arbid@devcode.se>
2021-03-04 10:07:00 +0100

    :hammer: seperate article form logic from articles and connect card logic

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,34 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './cardItem.module.css'
 import { ReactComponent as CloseIcon } from '../icons/close.svg'
 import ButtonComponent from './ButtonComponent'
-export default function CardItem() {
+import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
+import { remove, edit, toggleSeen } from '../features/articles/articlesSlice'
+
+CardItem.propTypes = {
+  id: PropTypes.string,
+  author: PropTypes.string,
+  body: PropTypes.string,
+  createdAt: PropTypes.string,
+  lastEdited: PropTypes.string,
+  seen: PropTypes.bool,
+  image: PropTypes.string,
+  delete: PropTypes.bool,
+  className: PropTypes.string
+}
+
+export default function CardItem(props) {
+  const dispatch = useDispatch()
+  const [toggleEditMode, setToggleEditMode] = useState(false)
+  const [EditText, setEditText] = useState({
+    id: '',
+    author: '',
+    body: '',
+    lastEdited: new Date().toDateString()
+  })
+  const handleEdit = ({ id, author, body }) => () => {
+    setToggleEditMode(true)
+    setEditText((prevState) => ({
+      ...prevState,
+      id: props.id,
+      author: props.author,
+      body: props.body
+    }))
+    setToggleEditMode(id)
+    setEditText((prevState) => ({ ...prevState, author, body }))
+  }
+  const handleSaveEdit = () => {
+    dispatch(edit(EditText))
+    setEditText(() => ({ id: '', author: '', body: '' }))
+    setToggleEditMode(false)
+  }
+
+  const handleToggleSeen = ({ id }) => {
+    dispatch(toggleSeen(id))
+  }
   return (
     <div className={styles.card}>
       <figure className={styles.figure}>
         <img src="https://picsum.photos/200/300" alt="" />
       </figure>
       <div>
-        <div className={styles.closeBtn}>
+        <div className={styles.closeBtn} onClick={() => dispatch(remove(props.id))}>
           <CloseIcon />
         </div>
         <div className={styles.cardTitle}>
-          <h3>Author title</h3>
+          <h3>{props.author}</h3>
         </div>
         <div className={styles.cardBody}>
-          <p>
-            Lorem ipsum dolor sit, amen consenter adipisicing elite. Laborer dolores, commode
-            dubious sapele laudanum delegati nebo quash mollitia
-          </p>
+          <p>{props.body}</p>
         </div>
         <hr className={styles.divider} />
         <div>
-          <p>created at 876876</p>
-          <p>last edited 09789798</p>
+          <span>
+            <p>created at: </p>
+            {props.createdAt ? <div>{props.createdAt}</div> : null}
+          </span>
+          <span>
+            <p>last edited:</p>
+            {props.lastEdited ? <div>{props.lastEdited}</div> : null}
+          </span>
         </div>
       </div>
       <div className={styles.cardFooter}>
-        <ButtonComponent>edit</ButtonComponent>
+        <ButtonComponent onClick={handleEdit(props)}>edit</ButtonComponent>
         {/* <ButtonComponent>seen</ButtonComponent> */}
       </div>
     </div>
--- src/features/articles/ArticleTest.js
+++ src/features/articles/ArticleTest.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { ArticleForm, CardItem } from '../../components'
+import { useSelector } from 'react-redux'
 import styles from './articleTest.module.css'
+import { getArticles } from './articlesSlice'
 
 export default function ArticleTest() {
+  const selectArticles = useSelector(getArticles)
+  const revMyArr = [].concat(selectArticles).reverse()
   return (
     <div>
       <div className={styles.formContainer}>
@@ -12,15 +16,21 @@ export default function ArticleTest() {
         <ArticleForm />
       </div>
       <div className={styles.container}>
-        <div className={styles.cardContainer}>
-          <CardItem />
-        </div>
-        <div className={styles.cardContainer}>
-          <CardItem />
-        </div>
-        <div className={styles.cardContainer}>
-          <CardItem />
-        </div>
+        {revMyArr?.map(({ id, lastEdited, createdAt, author, imgUrl, body, seen }) => {
+          return (
+            <div className={styles.cardContainer} key={id}>
+              <CardItem
+                lastEdited={lastEdited}
+                createdAt={createdAt}
+                author={author}
+                image={imgUrl}
+                body={body}
+                seen={seen}
+                id={id}
+              />
+            </div>
+          )
+        })}
       </div>
     </div>
   )
