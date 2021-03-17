8b64c50a8a7cb1c8bbf36de4eba5be6ef27881e7
Sam Arbid <sam.arbid@devcode.se>
2021-03-17 13:29:35 +0100

    :hammer:  display types in cardFooter

--- src/components/CardItem/CardFooter.js
+++ src/components/CardItem/CardFooter.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import DdChip from '../dropDownMenu/DdChip'
 
 import styles from './cardItem.module.css'
 
 CardFooter.propTypes = {
   createdAt: PropTypes.string,
   lastEdited: PropTypes.string,
-  seen: PropTypes.bool
+  seen: PropTypes.bool,
+  type: PropTypes.array
 }
 
-export default function CardFooter({ createdAt, lastEdited, seen }) {
+export default function CardFooter({ createdAt, lastEdited, seen, type }) {
   return (
     <>
       <hr className={styles.divider} />
@@ -18,7 +20,10 @@ export default function CardFooter({ createdAt, lastEdited, seen }) {
           <div>
             <div>Created at: {createdAt && createdAt}</div>
             <div>Last edited: {lastEdited && lastEdited}</div>
-            <div>{seen ? 'clicked' : 'not clicked'}</div>
+            <div className={styles.clicked}>{seen ? 'clicked' : 'not clicked'}</div>
+            {type?.map((t) => (
+              <DdChip key={t.key}>{t.title}</DdChip>
+            ))}
           </div>
         </div>
       </div>
